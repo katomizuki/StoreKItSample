26f71ce6df4cc2e8fa69bf1e7ab17fb03feb4036
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-07-25 16:34:13 +0900

    Initial Commit

---
+++ StoreKItSample/StoreKItSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKItSampleApp.swift
+//  StoreKItSample
+//
+//  Created by ミズキ on 2022/07/25.
+//
+
+import SwiftUI
+
+@main
+struct StoreKItSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
